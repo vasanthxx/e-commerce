6ec8e1c9254617cf721943a1b3b60ea1e85f9748
vasanth <109952961+vasanthparthasarathy1@users.noreply.github.com>
2025-02-20 10:07:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var side = document.getElementById('show')
+
+      function openmenu(){
+        side.style.right = "0";
+      }
+      function closemenu(){
+        side.style.right = "-300px";
+      }
+      
+
+      
